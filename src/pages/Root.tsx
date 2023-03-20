3bdde22ad1e97097dac9e829c0c04bf6a5ce4133
greg <ku392000@gmail.com>
2023-03-20 16:11:48 +0800

    Feat: Use responsive layout

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
 
 import FitnessEditor from '/src/components/FitnessEditor'
 import WeightChart from '/src/components/WeightChart'
@@ -12,22 +13,29 @@ const Root = () => {
 
   return (
     <>
-      <div className="h-16 fixed flex items-center w-full px-2 border-b border-gray-200 bg-white z-10">
+      <div className="h-16 fixed flex items-center w-full px-2 border-b border-gray-200 bg-white z-20">
         <img src="/vite.svg" className="w-10 h-10" alt="Vite logo" />
         <div className="text-3xl font-bold">
           Health App
         </div>
       </div>
       <div className="pt-16">
-        <div className="flex pt-2">
-          <div className="grow">
+        <div className="grid grid-cols-4 pt-2">
+          <div className="col-span-4 md:col-span-3 pr-8 md:pr-4">
+            <div className="sticky top-16 bg-white z-10 flex justify-end w-full">
+              <button className="p-4 md:hidden">
+                <PencilSquareIcon className="h-6 w-6" />
+              </button>
+            </div>
             <WeightChart data={data} height={400} />
             <BodyFatChart data={data} height={400} />
             <MuscleChart data={data} height={400} />
           </div>
-          <div className="w-1/4 px-2">
-            <div className="text-xl font-bold">編輯</div>
-            <FitnessEditor />
+          <div className="px-2 hidden md:block">
+            <div className="sticky top-16">
+              <div className="text-xl font-bold">編輯</div>
+              <FitnessEditor />
+            </div>
           </div>
         </div>
       </div>
