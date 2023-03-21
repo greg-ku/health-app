0df0783df280ccc9102bae033e61fef68c78640d
greg <ku392000@gmail.com>
2023-03-21 20:59:27 +0800

    Fix: Refine base path for assets

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -21,7 +21,7 @@ const Root = () => {
   return (
     <>
       <div className="h-16 fixed flex items-center w-full px-2 border-b border-gray-200 bg-white z-20">
-        <img src="/vite.svg" className="w-10 h-10" alt="Vite logo" />
+        <img src={`${import.meta.env.BASE_URL}/vite.svg`} className="w-10 h-10" alt="Vite logo" />
         <div className="text-3xl font-bold">
           Health App
         </div>
